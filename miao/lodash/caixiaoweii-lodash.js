209bdf72d0fe7c5678e57b4b8c9d161f3fa11f73
Administrator <1208296574@qq.com>
2020-12-08 16:32:35 +0800

    chunk compact

--- miao/lodash/caixiaoweii-lodash.js
+++ miao/lodash/caixiaoweii-lodash.js
@@ -1,5 +1,40 @@
-var caixiaoweii = {
+var caixiaoweii = function () {
+  function compact(ary) {
+    var result = []
+    for (let i = 0; i < ary.length; i++) {
+      if (ary[i]) {
+        result.push(ary[i])
+      }
+    }
+    return result
+  }
+  function chunk(ary, size = 1) {
+    if (!ary || ary.length < 0) {
+      return []
+    }
+    var result = []
+    var i = 0
+    while (true) {
+      result.push(ary.slice(i, i + size));
+      i += size;
+      if (i > ary.length) {
+        break
+      }
+    }
+    return result
+
+  }
 
 
 
-}
+
+
+  return {
+    compact,
+    chunk,
+  }
+
+}()
+
+
+
